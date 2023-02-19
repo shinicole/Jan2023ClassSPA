d1677f7e4e069da84f02e81b45e1093f0243e1df
Shivon Talavera <t.shivon@yahoo.com>
2023-02-18 18:13:58 -0600

    Jan2023ClassSpa

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+//document.querySelector(".fa-bars").addEventListener("click", () => {
+// document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+//});
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
+  ${Footer()}
+  `;
+  router.updatePageLinks();
+}
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    }
+  })
+  .resolve();
